3be18709a5e9b49665429f71b97085fdcc4c8124
katayoungh <katatayoungh@gmail.com>
2023-08-19 20:16:00 +0330

    login and member course change

--- src/ui/Card.js
+++ src/ui/Card.js
@@ -1,12 +1,14 @@
 import classes from './style/Card.module.css';
 
-function Card({ children, profile, editProfile, courseInfo, changePass }) {
+function Card({ children, profile, editProfile, courseInfo, changePass,login,courseMember,members }) {
     let cardStyle;
+    if (login) cardStyle = classes.loginStyle;
     if (profile) cardStyle = classes.profileStyle;
     if (editProfile) cardStyle = classes.editProfileStyle;
     if (changePass) cardStyle = classes.changePassStyle;
     if (courseInfo) cardStyle = classes.courseStyle;
-
+    if (courseMember) cardStyle = classes.courseMemberStyle;
+    if (members) cardStyle = classes.membersStyle;
     return (
         <div className={cardStyle}>{children}</div>
     );
